d45831cecf56a81100b96181a948d829c061a0b5
ArneHoffhues <72792110+ArneHoffhues@users.noreply.github.com>
2021-02-10 14:08:28 +0100

    added KITTI MOTS and MOTS Challenge datasets

--- hota_metrics/datasets/_base_dataset.py
+++ hota_metrics/datasets/_base_dataset.py
@@ -4,6 +4,7 @@ import zipfile
 import os
 import traceback
 import numpy as np
+from pycocotools import mask as mask_utils
 from copy import deepcopy
 from abc import ABC, abstractmethod
 from .. import _timing
@@ -204,6 +205,35 @@ class _BaseDataset(ABC):
                 'File %s cannot be read because it is either not present or invalidly formatted' % os.path.basename(
                     file))
         return read_data, crowd_ignore_data
+
+    @staticmethod
+    def _calculate_mask_ious(masks1, masks2, is_encoded=False, do_ioa=False):
+        """ Calculates the IOU (intersection over union) between two arrays of segmentation masks.
+        If is_encoded a run length encoding with pycocotools is assumed as input format, otherwise an input of numpy
+        arrays of the shape (num_masks, height, width) is assumed and the encoding is performed.
+        If do_ioa (intersection over area) , then calculates the intersection over the area of masks1 - this is commonly
+        used to determine if detections are within crowd ignore region.
+        :param masks1:  first set of masks (numpy array of shape (num_masks, height, width) if not encoded,
+                        else pycocotools rle encoded format)
+        :param masks2:  second set of masks (numpy array of shape (num_masks, height, width) if not encoded,
+                        else pycocotools rle encoded format)
+        :param is_encoded: whether the input is in pycocotools rle encoded format
+        :param do_ioa: whether to perform IoA computation
+        :return: the IoU/IoA scores
+        """
+        # use pycocotools for run length encoding of masks
+        if not is_encoded:
+            masks1 = mask_utils.encode(np.array(np.transpose(masks1, (1, 2, 0)), order='F'))
+            masks2 = mask_utils.encode(np.array(np.transpose(masks2, (1, 2, 0)), order='F'))
+
+        # use pycocotools for iou computation of rle encoded masks
+        ious = np.asarray(mask_utils.iou(masks1, masks2, [do_ioa for _ in range(len(masks1))]))
+        if len(masks1) == 0 or len(masks2) == 0:
+            ious = ious.reshape(len(masks1), len(masks2))
+        assert (ious >= 0).all()
+        assert (ious <= 1).all()
+
+        return ious
 
     @staticmethod
     def _calculate_box_ious(bboxes1, bboxes2, box_format='xywh', do_ioa=False):
