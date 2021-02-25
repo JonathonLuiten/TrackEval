4ef3eafbec474cdb6aac0aaba530075767339316
ArneHoffhues <72792110+ArneHoffhues@users.noreply.github.com>
2021-02-25 15:45:08 +0100

    added DAVIS dataset evaluation + J&F metrics (#10)
    * added Kitti MOTS and MOTChallenge MOTS dataset
    
    * added J&F metric and DAVIS dataset evaluation
    
    * added bdd100k dataset evaluation
    changed eval structure to allow for super category evaluation
    
    * added config option to optimize either on J or J&F measure
    
    * optimization of davis evaluation with minor data format changes
    
    * optimization of davis evaluation with minor data format changes
    
    * reworked mot challenge mask evaluation
    
    * reworked kitti mots evaluation
    
    * updated mot mots run script
    
    * added test case for mots
    
    * changed naming
    
    * fixed mots test to fit to the naming changes in mots dataset files
    
    * adapted eval to also include combined class evaluation
    
    * reworked bdd100k evaluation and added commentary
    
    * minor code style fixes
    
    * reworked davis dataset
    
    * adapted davis dataset to most recent format + added commentary
    added class averaging functions for J&F metrics
    
    * minor commentary and code style fixes
    
    * minor fix in clear metrics class averaging
    
    * minor bug fix

--- scripts/run_davis.py
+++ scripts/run_davis.py
@@ -1,8 +1,7 @@
-
-""" run_bdd.py
+""" run_davis.py
 
 Run example:
-run_bdd.py --USE_PARALLEL False --METRICS Hota --TRACKERS_TO_EVAL qdtrack
+run_davis.py --USE_PARALLEL False --METRICS HOTA --TRACKERS_TO_EVAL ags
 
 Command Line Arguments: Defaults, # Comments
     Eval arguments:
@@ -17,20 +16,23 @@ Command Line Arguments: Defaults, # Comments
         'OUTPUT_DETAILED': True,
         'PLOT_CURVES': True,
     Dataset arguments:
-            'GT_FOLDER': os.path.join(code_path, 'data/gt/bdd100k/bdd100k_val'),  # Location of GT data
-            'TRACKERS_FOLDER': os.path.join(code_path, 'data/trackers/bdd100k/bdd100k_val'),  # Trackers location
-            'OUTPUT_FOLDER': None,  # Where to save eval results (if None, same as TRACKERS_FOLDER)
-            'TRACKERS_TO_EVAL': None,  # Filenames of trackers to eval (if None, all in folder)
-            'CLASSES_TO_EVAL': ['pedestrian', 'rider', 'car', 'bus', 'truck', 'train', 'motorcycle', 'bicycle'],
-            # Valid: ['pedestrian', 'rider', 'car', 'bus', 'truck', 'train', 'motorcycle', 'bicycle']
-            'SPLIT_TO_EVAL': 'val',  # Valid: 'training', 'val',
-            'INPUT_AS_ZIP': False,  # Whether tracker input files are zipped
-            'PRINT_CONFIG': True,  # Whether to print current config
-            'TRACKER_SUB_FOLDER': 'data',  # Tracker files are in TRACKER_FOLDER/tracker_name/TRACKER_SUB_FOLDER
-            'OUTPUT_SUB_FOLDER': '',  # Output files are saved in OUTPUT_FOLDER/tracker_name/OUTPUT_SUB_FOLDER
-            'TRACKER_DISPLAY_NAMES': None,  # Names of trackers to display, if None: TRACKERS_TO_EVAL
+    '   'GT_FOLDER': os.path.join(code_path, 'data/gt/davis/'),  # Location of GT data
+        'TRACKERS_FOLDER': os.path.join(code_path, 'data/trackers/davis/davis_val'),  # Trackers location
+        'OUTPUT_FOLDER': None,  # Where to save eval results (if None, same as TRACKERS_FOLDER)
+        'TRACKERS_TO_EVAL': None,  # Filenames of trackers to eval (if None, all in folder)
+        'SPLIT_TO_EVAL': 'val',  # Valid: 'val', 'train'
+        'PRINT_CONFIG': True,  # Whether to print current config
+        'TRACKER_SUB_FOLDER': 'data',  # Tracker files are in TRACKER_FOLDER/tracker_name/TRACKER_SUB_FOLDER
+        'OUTPUT_SUB_FOLDER': '',  # Output files are saved in OUTPUT_FOLDER/tracker_name/OUTPUT_SUB_FOLDER
+        'TRACKER_DISPLAY_NAMES': None,  # Names of trackers to display, if None: TRACKERS_TO_EVAL
+        'SEQMAP_FOLDER': None,  # Where seqmaps are found (if None, GT_FOLDER/ImageSets/2017)
+        'SEQMAP_FILE': None,  # Directly specify seqmap file (if none use seqmap_folder/split-to-eval.txt)
+        'SEQ_INFO': None,  # If not None, directly specify sequences to eval and their number of timesteps
+        'GT_LOC_FORMAT': '{gt_folder}/Annotations_unsupervised/480p/{seq}',
+        # '{gt_folder}/Annotations_unsupervised/480p/{seq}'
+        'MAX_DETECTIONS': 0  # Maximum number of allowed detections per sequence (0 for no threshold)
     Metric arguments:
-        'METRICS': ['Hota','Clear', 'ID', 'Count']
+        'METRICS': ['HOTA', 'CLEAR', 'Identity', 'JAndF']
 """
 
 import sys
@@ -46,9 +48,8 @@ if __name__ == '__main__':
 
     # Command line interface:
     default_eval_config = trackeval.Evaluator.get_default_eval_config()
-    default_eval_config['PRINT_ONLY_COMBINED'] = True
-    default_dataset_config = trackeval.datasets.BDD100K.get_default_dataset_config()
-    default_metrics_config = {'METRICS': ['HOTA', 'CLEAR', 'Identity']}
+    default_dataset_config = trackeval.datasets.DAVIS.get_default_dataset_config()
+    default_metrics_config = {'METRICS': ['HOTA', 'CLEAR', 'Identity', 'JAndF']}
     config = {**default_eval_config, **default_dataset_config, **default_metrics_config}  # Merge default configs
     parser = argparse.ArgumentParser()
     for setting in config.keys():
@@ -79,9 +80,9 @@ if __name__ == '__main__':
 
     # Run code
     evaluator = trackeval.Evaluator(eval_config)
-    dataset_list = [trackeval.datasets.BDD100K(dataset_config)]
+    dataset_list = [trackeval.datasets.DAVIS(dataset_config)]
     metrics_list = []
-    for metric in [trackeval.metrics.HOTA, trackeval.metrics.CLEAR, trackeval.metrics.Identity]:
+    for metric in [trackeval.metrics.HOTA, trackeval.metrics.CLEAR, trackeval.metrics.Identity, trackeval.metrics.JAndF]:
         if metric.get_name() in metrics_config['METRICS']:
             metrics_list.append(metric())
     if len(metrics_list) == 0:
