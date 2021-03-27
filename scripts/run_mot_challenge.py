9da41a0c8b70eefe45e5d43286f75b40da9d7d5a
Jack Valmadre <valmadre@google.com>
2021-03-27 15:45:39 +0100

    Use array_fields for IOU and recall in detection

--- scripts/run_mot_challenge.py
+++ scripts/run_mot_challenge.py
@@ -80,7 +80,8 @@ if __name__ == '__main__':
     evaluator = trackeval.Evaluator(eval_config)
     dataset_list = [trackeval.datasets.MotChallenge2DBox(dataset_config)]
     metrics_list = []
-    for metric in [trackeval.metrics.HOTA, trackeval.metrics.CLEAR, trackeval.metrics.Identity, trackeval.metrics.VACE, trackeval.metrics.CLEARDet]:
+    for metric in [trackeval.metrics.HOTA, trackeval.metrics.CLEAR, trackeval.metrics.Identity, trackeval.metrics.VACE,
+                   trackeval.metrics.Detection, trackeval.metrics.DetectionConfidence]:
         if metric.get_name() in metrics_config['METRICS']:
             metrics_list.append(metric())
     if len(metrics_list) == 0:
