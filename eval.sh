e85422b364b436d2aaabe61a11e65b0b334894ef
son <leduyson2632001@gmail.com>
2021-09-07 22:54:40 +0700

    add evaluate script

---
+++ eval.sh
@@ -0,0 +1,3 @@
+python scripts/run_mot_challenge.py --BENCHMARK MOT16 --SPLIT_TO_EVAL test --TRACKERS_TO_EVAL ch_yolov5m_deep_sort --METRICS HOTA CLEAR Identity --EXTRACTOR FP --HEATMAP GT PRED IDSW --ID_SWITCH True  
+ 
+	
