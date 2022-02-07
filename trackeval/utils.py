ff513440a705ade92e1fd38d64f13e7daedaed99
Alexander Sing <AlexanderSing@live.de>
2022-02-07 16:38:24 +0100

    Fixed 3D IoU calc

--- trackeval/utils.py
+++ trackeval/utils.py
@@ -2,7 +2,6 @@
 import os
 import csv
 import argparse
-import numpy as np
 from collections import OrderedDict
 
 
@@ -145,10 +144,3 @@ def load_detail(file):
 class TrackEvalException(Exception):
     """Custom exception for catching expected errors."""
     ...
-
-def box3d_vol(corners):
-    """ corners: (8,3) no assumption on axis direction """
-    a = np.sqrt(np.sum((corners[0, :] - corners[1, :]) ** 2))
-    b = np.sqrt(np.sum((corners[1, :] - corners[2, :]) ** 2))
-    c = np.sqrt(np.sum((corners[0, :] - corners[4, :]) ** 2))
-    return a * b * c
