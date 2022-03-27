f30223749c72d135411eeeaba454e06622b1a987
Yang Liu <lylander14@gmail.com>
2022-03-28 01:33:54 +0800

    add evaluation for tao-ow dataset (#54)
    * add evaluation for tao-ow dataset
    
    * Update tao_ow.py
    
    expand `all_ids` to maximum of 2000 ids.
    
    * update distractor class ids

--- trackeval/datasets/__init__.py
+++ trackeval/datasets/__init__.py
@@ -5,6 +5,7 @@ from .mots_challenge import MOTSChallenge
 from .bdd100k import BDD100K
 from .davis import DAVIS
 from .tao import TAO
+from .tao_ow import TAO_OW
 from .youtube_vis import YouTubeVIS
 from .head_tracking_challenge import HeadTrackingChallenge
 from .rob_mots import RobMOTS
